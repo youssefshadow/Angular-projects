33e1ff2faa67d0dccfa632a7c0be75b3902ae613
youssefshadow <youssef20ben@gmail.com>
2023-05-03 09:53:59 +0200

    disable_construct

--- src/app/les-amis/les-amis.component.ts
+++ src/app/les-amis/les-amis.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
   styleUrls: ['./les-amis.component.css'],
 })
 export class LesAmisComponent {
-  authorizeAmi = false;
+  authorizeAmi: boolean = false;
 
   constructor() {
     setTimeout(() => {
