b2a70c8a38ecbc70c8fd96caa17daa0db76588c3
youssefshadow <youssef20ben@gmail.com>
2023-05-04 10:21:04 +0200

    ng style dynamique

--- src/app/les-amis/les-amis.component.ts
+++ src/app/les-amis/les-amis.component.ts
@@ -10,24 +10,44 @@ export class LesAmisComponent {
   lesAmisCreationStatus: string = 'Patientez';
   amiName: string = '';
   amiCreated: boolean = false;
+  unAmiStatus: string = '';
+  titleColor: string = '';
 
   constructor() {
     setTimeout(() => {
       this.authorizeAmi = true;
       this.lesAmisCreationStatus = 'Cliquer pour ajouter un ami 😼!';
+      if (this.amiName === 'mathieu') {
+        this.unAmiStatus = 'ON';
+        this.titleColor = '#F97B22';
+      } else {
+        this.unAmiStatus = 'OFF';
+        this.titleColor = '#41644A';
+      }
     }, 3000);
   }
 
   onCreationAmi() {
     if (this.amiName !== '') {
       this.lesAmisCreationStatus = `Un nouvel ami a été ajouté: ${this.amiName}  😅 `;
-
       this.amiCreated = true;
+      if (this.amiName === 'mathieu') {
+        this.unAmiStatus = 'ON';
+        this.titleColor = '#F97B22';
+      } else {
+        this.unAmiStatus = 'OFF';
+        this.titleColor = '#41644A';
+      }
     }
   }
 
   onUpdateAmiName(event: any) {
     console.log(event.target.value);
     this.amiName = event.target.value;
+  }
+
+  onUpdateTitleColor(event: any) {
+    console.log(event.target.value);
+    this.titleColor = event.target.value;
   }
 }
