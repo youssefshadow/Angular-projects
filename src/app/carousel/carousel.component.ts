76946ee926864a7225c75588aedd655ffa6341ac
youssefshadow <youssef20ben@gmail.com>
2023-05-09 09:59:12 +0200

    slide_composant

--- src/app/carousel/carousel.component.ts
+++ src/app/carousel/carousel.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-carousel',
   templateUrl: './carousel.component.html',
-  styleUrls: ['./carousel.component.css']
+  styleUrls: ['./carousel.component.css'],
 })
 export class CarouselComponent {
+  @Input() images: string[] = [];
+  currentSlide = 0;
 
+  nextSlide() {
+    this.currentSlide = (this.currentSlide + 1) % this.images.length;
+  }
+
+  prevSlide() {
+    this.currentSlide =
+      (this.currentSlide + this.images.length - 1) % this.images.length;
+  }
 }
