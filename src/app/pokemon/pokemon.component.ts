d0f08c4255540f03ae71f0eca1c36a8e2b8150be
youssefshadow <youssef20ben@gmail.com>
2023-05-09 16:21:43 +0200

    fvoris

--- src/app/pokemon/pokemon.component.ts
+++ src/app/pokemon/pokemon.component.ts
@@ -10,6 +10,8 @@ export class PokemonComponent implements OnInit {
   filteredPokemonList: any[] = [];
   searchTerm: string = '';
   noResultsFound: boolean = false;
+  favoritePokemonList: any[] = [];
+  showFavoritesOnly = false;
 
   constructor() {}
 
@@ -34,5 +36,28 @@ export class PokemonComponent implements OnInit {
       return pokemon.name.toLowerCase().includes(this.searchTerm.toLowerCase());
     });
     this.noResultsFound = this.filteredPokemonList.length === 0; // set noResultsFound to true if no pokemon matches the search term
+  }
+
+  addToFavorites(pokemon: any) {
+    if (!this.favoritePokemonList.includes(pokemon)) {
+      this.favoritePokemonList.push(pokemon);
+    }
+  }
+
+  removeFromFavorites(pokemon: any) {
+    const index = this.favoritePokemonList.indexOf(pokemon);
+    if (index >= 0) {
+      this.favoritePokemonList.splice(index, 1);
+    }
+  }
+
+  isFavorite(pokemon: any) {
+    return this.favoritePokemonList.includes(pokemon);
+  }
+  toggleFavorites() {
+    this.showFavoritesOnly = !this.showFavoritesOnly;
+    this.filteredPokemonList = this.showFavoritesOnly
+      ? this.favoritePokemonList
+      : this.pokemonList;
   }
 }
